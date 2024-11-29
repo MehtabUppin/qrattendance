7b9344d23a7a325bb20cf8f9ba5b533c69903262
Mehtab Kausar <168742140+MehtabUppin@users.noreply.github.com>
2024-11-29 16:17:17 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:8080/api/login', { email, password });
+            const response = await axios.post('https://qrcode-vrh2.onrender.com/api/login', { email, password });
             const data = response.data;
 
             // Save token, user ID, and role to local storage
